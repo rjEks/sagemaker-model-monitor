adb88e51164bbf36021ec73aba0e403de18a3339
rjEks <rjekstein@gmail.com>
2021-10-21 10:51:22 -0300

    Update sagemaker-data-catpured.py

--- sagemaker-data-catpured.py
+++ sagemaker-data-catpured.py
@@ -1,5 +1,6 @@
 import boto3
 from sagemaker_monitor import Settings
+import json
 
 
 data_capture_prefix = "data_capture_prefix"
@@ -16,3 +17,14 @@ def view_capture_data():
     
     print("Arquivos Capturados:")
     print("\n ".join(capture_files))
+    
+    get_obj_body(capture_files[-1])
+    
+    print("Check Primeiras Linhas")
+    print(capture_file[:10])
+    
+ def get_obj_body(obj_key):
+     return s3_client.get_object(Bucket=bucket, Key=obj_key).get("Body").read().decode("urf-8")
+    
+    #capture_file = get_obj_body(capture_files[-1])
+    #print(capture_file[:2000])
